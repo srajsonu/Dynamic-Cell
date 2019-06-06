9ea474a688b681416edfc6bd8cd0995b1b5931df
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-06-06 17:42:28 +0530

    Initial Commit

---
+++ Cell/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Cell
+//
+//  Created by ARY@N on 06/06/19.
+//  Copyright © 2019 ARYAN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
